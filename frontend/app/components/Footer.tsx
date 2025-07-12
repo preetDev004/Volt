51b5324a4ec28566a917a9c20f272a8b068ec271
Preet Patel <preet.dev@projecthumancity.com>
2025-07-11 21:55:39 -0400

    removed the v1 code for clean v2 start.

--- frontend/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="border-t border-purple-500/10 py-12">
-      <div className="mt-12 border-t border-purple-500/10 pt-8 text-center text-gray-400">
-        <p>&copy; 2024 Volt. All rights reserved. Created with 💜 by Preet</p>
-      </div>
-    </footer>
-  );
-}
