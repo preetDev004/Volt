ec64256548d832fd5a4e82d59cdc59565fb59bb7
Preet 🚀 <preetpatelofficial004@gmail.com>
2025-07-30 14:24:17 -0400

    test: running a nextjs app through the sandbox template and accessing it

--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,11 +1,19 @@
 import { createAgent, openai } from '@inngest/agent-kit';
 import { inngest } from './client';
+import { Sandbox } from '@e2b/code-interpreter';
+import { getSandbox } from './utils';
 
 // test inngest function
 export const helloWorld = inngest.createFunction(
   { id: 'hello-world' },
   { event: 'test/hello.world' },
   async ({ event, step }) => {
+    const sandbox_Id = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('volt-nextjs-test');
+
+      return sandbox.sandboxId;
+    });
+
     // Create a new agent with a system prompt (you can add optional tools, too)
     const codeAgent = createAgent({
       name: 'code-agent',
@@ -18,8 +26,14 @@ export const helloWorld = inngest.createFunction(
       `Write code for: ${event.data.text}`
     );
 
-    console.log(output);
+    const sandboxUrl = await step.run('get-sandbox-url', async () => {
+      const sandbox = await getSandbox(sandbox_Id);
+      // get the url of the sandbox on port 3000 (as Next.js app runs on port 3000)
+      const host = sandbox.getHost(3000);
 
-    return { message: output };
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
