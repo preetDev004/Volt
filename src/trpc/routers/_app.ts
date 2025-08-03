e7ec647cc5b333e4c71261152d7deef346dcb970
Preet 🚀 <preetpatelofficial004@gmail.com>
2025-08-03 18:06:31 -0400

    feat: added basic create message procedure for test

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,37 +1,8 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { inngest } from '../../inngest/client';
-import { NextResponse } from 'next/server';
+import { messagesRouter } from '@/trpc/procedures/messages/procedure';
+import { createTRPCRouter } from '../init';
 
 export const appRouter = createTRPCRouter({
-  greet: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query(opts => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
-    }),
-
-  inngest_test: baseProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .mutation(async ({ input }) => {
-      await inngest.send({
-        name: 'test/hello.world',
-        data: {
-          text: input.text,
-        },
-      });
-
-      return { message: 'Event sent!' };
-    }),
+  messages: messagesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
